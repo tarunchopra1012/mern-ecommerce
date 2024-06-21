f77248435a6136f8c19072b0c33261dddfd7df30
tarunchopra1012 <tarun.chopra10@gmail.com>
2024-06-21 07:34:23 +0530

    Created migration script to enter 1 million records through batch processing.

--- backend/seedProducts.js
+++ backend/seedProducts.js
@@ -11,36 +11,36 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-async function insertProducts() {
-  const products = [];
+async function insertProductsInBatches(batchSize, batches) {
+  for (let batch = 0; batch < batches; batch++) {
+    const products = [];
+    for (let i = 0; i < batchSize; i++) {
+      products.push({
+        title: faker.commerce.productName(),
+        description: faker.commerce.productDescription(),
+        price: parseFloat(faker.commerce.price()),
+        discountPercentage: faker.number.float({
+          multipleOf: 0.01,
+          min: 0,
+          max: 30,
+        }),
+        rating: faker.number.float({ multipleOf: 0.1, min: 0, max: 5 }),
+        stock: faker.number.int({ min: 10, max: 100 }),
+        brand: faker.company.name(),
+        category: faker.commerce.department(),
+        thumbnail: faker.image.url(),
+        images: faker.image.url(),
+      });
+    }
 
-  for (let i = 0; i < 1000; i++) {
-    products.push({
-      title: faker.commerce.productName(),
-      description: faker.commerce.productDescription(),
-      price: parseFloat(faker.commerce.price()),
-      discountPercentage: faker.number.float({
-        multipleOf: 0.01,
-        min: 0,
-        max: 30,
-      }),
-      rating: faker.number.float({ multipleOf: 0.1, min: 0, max: 5 }),
-      stock: faker.number.int({ min: 10, max: 100 }),
-      brand: faker.company.name(),
-      category: faker.commerce.department(),
-      thumbnail: faker.image.url(),
-      images: faker.image.url(),
-    });
+    try {
+      await Product.insertMany(products);
+      console.log(`Batch ${batch + 1} inserted`);
+    } catch (error) {
+      console.error("Error inserting products:", error);
+    }
   }
-
-  try {
-    await Product.insertMany(products);
-    console.log("Products inserted"); // Success
-  } catch (error) {
-    console.error("Error inserting products:", error);
-  }
-
   mongoose.disconnect();
 }
 
-insertProducts();
+insertProductsInBatches(1000, 1000);
